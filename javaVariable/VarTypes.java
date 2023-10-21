77cf1c8b7d70d90e19254651567fe9658d3acbdd
Khumendra Sinha <khumendrasinha78@gmail.com>
2023-10-21 10:56:46 +0530

    add varTypes

---
+++ javaVariable/VarTypes.java
@@ -0,0 +1,11 @@
+// local, instance and static variable
+
+class VarTypes{
+ static int m = 100; // static variable
+ void method(){
+    int n = 90; // local variable
+ }
+ public static void main(String args[]){
+    int data = 50; // instance variable
+ }
+}// end of class
