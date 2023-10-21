6c076d0af3c6d3c00058ddb57502cdea64f1c36c
Khumendra Sinha <khumendrasinha78@gmail.com>
2023-10-21 10:38:10 +0530

    java variable example

---
+++ javaVariable/Variable.java
@@ -0,0 +1,7 @@
+// Create a variable named carName and assign the value Volvo to it.
+public class Variable{
+    public static void main(String[] args){
+        String name = "Vinod";
+        System.out.println( name);
+    }
+}
